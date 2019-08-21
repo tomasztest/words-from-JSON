5ad6454fa7ff85942ba9026897ea17e4c443978d
tomasztest <tomaszkotzur@gmail.com>
2019-08-21 10:30:53 +0200

    Updates

---
+++ precache-manifest.6a4ddbc1cd2578ba52c5ac03a76e3715.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3085300a0dac623dc4ef144d2a63a683",
+    "url": "/words-from-JSON/index.html"
+  },
+  {
+    "revision": "bff6665893c0c1a47377",
+    "url": "/words-from-JSON/static/css/main.349e317b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/words-from-JSON/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "bff6665893c0c1a47377",
+    "url": "/words-from-JSON/static/js/main.56554b9c.chunk.js"
+  },
+  {
+    "revision": "c649ef2f786ffa6f03b1",
+    "url": "/words-from-JSON/static/js/runtime~main.7b22cfe3.js"
+  }
+]);
